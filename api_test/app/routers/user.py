0ac7b974619536e3a557ec55e2fc1c3082f91202
yskim <spro1@naver.com>
2022-08-29 10:56:11 +0900

    init commit

---
+++ api_test/app/routers/user.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from bson.objectid import ObjectId
+from app.serializers.userSerializers import userResponseEntity
+
+from app.database import User
+from .. import schemas, oauth2
+
+router = APIRouter()
+
+
+@router.get('/info', response_model=schemas.UserResponse)
+def get_me(user_id: str = Depends(oauth2.require_user)):
+    print(User.find_one({'_id': ObjectId(str(user_id))}))
+    user = userResponseEntity(User.find_one({'_id': ObjectId(str(user_id))}))
+    return {"status": "success", "user": user}
---
+++ api_test/readMe.md
@@ -0,0 +1,1 @@
+# Mongodb & FastAPI JWT Test
